1dec0ee0d0e91d130dee0aec7ab728595992dbde
narrakos <narrakos@freemail.hu>
2020-12-19 21:26:53 +0100

    Header váz

---
+++ sulyemelo-frontend/src/components/header/TitleBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const TitleBar = () => {
+    return (
+        <div>
+            <React.Fragment>
+
+                <img src="" alt="logo"/>
+                <h1>Pécsi Súlyemelő Egyesület</h1>
+                <div>
+                    <img src="" alt="facebook"/>
+                    <img src="" alt="youtube"/>
+                    <img src="" alt="twitter"/>
+                    <img src="" alt="instagram"/>
+                </div>
+            </React.Fragment>
+        </div>
+    );
+};
+
+export default TitleBar;
