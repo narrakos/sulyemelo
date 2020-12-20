5bf12fe51a8b284a4f63a8d59839e2872bfaad83
marczin-eiler <eiler.viki@gmail.com>
2020-12-20 17:24:55 +0100

    Service-ek létrehozása

---
+++ sulyemelo-backend/src/main/java/com/backend/service/EventService.java
@@ -0,0 +1,8 @@
+package com.backend.service;
+
+import com.backend.entity.Event;
+import com.backend.repository.EventRepository;
+
+public class EventService extends AbstractService<Event, EventRepository>{
+
+}
