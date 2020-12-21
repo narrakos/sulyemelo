331e01853d4bf4cc1ff2a71540db3da6d018d9b9
narrakos <narrakos@freemail.hu>
2020-12-21 16:53:45 +0100

    navbar div

--- sulyemelo-frontend/src/components/header/Navbar.js
+++ sulyemelo-frontend/src/components/header/Navbar.js
@@ -3,17 +3,19 @@ import {BrowserRouter as Router, Link} from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <React.Fragment>
-            <Router>
-                <Link to="#">Home</Link>
-                <Link to="#">Rólunk</Link>
-                <Link to="#">Tagok</Link>
-                <Link to="#">Versenyek</Link>
-                <Link to="#">Galéria</Link>
-                <Link to="#">Dokumentumtár</Link>
-                <Link to="#">Kapcsolat</Link>
-            </Router>
-        </React.Fragment>
+        <div>
+            <React.Fragment>
+                <Router>
+                    <Link to="#">Home</Link>
+                    <Link to="#">Rólunk</Link>
+                    <Link to="#">Tagok</Link>
+                    <Link to="#">Versenyek</Link>
+                    <Link to="#">Galéria</Link>
+                    <Link to="#">Dokumentumtár</Link>
+                    <Link to="#">Kapcsolat</Link>
+                </Router>
+            </React.Fragment>
+        </div>
     );
 };
 
