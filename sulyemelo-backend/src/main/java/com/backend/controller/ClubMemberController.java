f2101510f43e24a1cf6a4257ff786fd9bbca0f7f
tyr0996 <galmar05aszi@gmail.com>
2020-12-21 20:05:55 +0100

    DocumentController és ClubMemberController kamuadatok törlése DocumentController és ClubMemberController Service-s metódusok használata

--- sulyemelo-backend/src/main/java/com/backend/controller/ClubMemberController.java
+++ sulyemelo-backend/src/main/java/com/backend/controller/ClubMemberController.java
@@ -23,32 +23,7 @@ public class ClubMemberController {
 
     @GetMapping("getAll")
     public ArrayList<ClubMember> getClubMembers(){
-        ClubMember cm1 = new ClubMember();
-        ClubMember cm2 = new ClubMember();
-        ClubMember cm3 = new ClubMember();
-
-        cm1.setName("cm1Nev");
-        cm1.setWeightClass(1);
-        cm1.setBirthdate(LocalDate.now());
-        cm1.setBestCleanAndJerk(11);
-        cm1.setBestSnatch(111);
-        cm2.setName("cm2Nev");
-        cm2.setWeightClass(2);
-        cm2.setBirthdate(LocalDate.now());
-        cm2.setBestCleanAndJerk(22);
-        cm2.setBestSnatch(222);
-        cm3.setName("cm3Nev");
-        cm3.setWeightClass(3);
-        cm3.setBirthdate(LocalDate.now());
-        cm3.setBestCleanAndJerk(33);
-        cm3.setBestSnatch(33);
-
-        ArrayList<ClubMember> clubMembers = new ArrayList<>();
-        clubMembers.add(cm1);
-        clubMembers.add(cm2);
-        clubMembers.add(cm3);
-
-        return clubMembers;
+        return (ArrayList<ClubMember>) this.clubMemberService.findAll();
     }
 
     @PostMapping("save")
